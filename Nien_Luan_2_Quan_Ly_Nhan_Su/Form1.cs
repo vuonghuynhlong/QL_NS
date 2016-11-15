b1f0f4cdec40e0d7774c18ec969d86fcbff714b3
vuonghuynhlong <vuonghuynhlong@gmail.com>
2016-11-15 20:54:17 +0700

    1 Commit

---
+++ Nien_Luan_2_Quan_Ly_Nhan_Su/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nien_Luan_2_Quan_Ly_Nhan_Su
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Nien_Luan_2_Quan_Ly_Nhan_Su/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nien_Luan_2_Quan_Ly_Nhan_Su
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
